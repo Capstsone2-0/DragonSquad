9adef7f7063a597ed6bccd03d77cab64c3d37407
karyRmartinez <karymartinez@pursuit.org>
2023-10-23 14:19:33 -0400

    Added getters,setter and constructors in Transaction class

---
+++ AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+       // Ledger ledger = new Ledger();
+        System.out.println("Hello world!");
+    }
+}
