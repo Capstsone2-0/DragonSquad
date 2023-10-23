dd67f62398ae1c0027c59bc5597c1073bcb2e977
karyRmartinez <karymartinez@pursuit.org>
2023-10-23 15:42:25 -0400

    new class Deposit to inherit attributes from transaction

--- AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/Transaction.java
+++ AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/Transaction.java
@@ -7,6 +7,7 @@ public class Transaction {
     private String description;
     private String vendor;
     private double amount;
+    private String type;
 
     public LocalDate getDate() {
         return date;
@@ -28,11 +29,36 @@ public class Transaction {
         return amount;
     }
 
-    public void transaction(LocalDate date, LocalTime time, String description, String vendor, Double amount) {
+    public Transaction(LocalDate date, LocalTime time, String description, String vendor, Double amount) {
         this.date = date;
         this.time = time;
         this.description = description;
         this.vendor = vendor;
         this.amount = amount;
+
+
+
+        if (amount > 0) {
+            this.type = "Deposit";
+        }else{
+            this.type = "payment";
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return date + "|" + time + "|" + description + "|" + vendor + "|" + amount;
+    }
+
+    class Deposit extends Transaction {
+        public Deposit(LocalDate date, LocalTime time, String description, String vendor, double amount) {
+            super(date, time, description, vendor, amount);
+        }
+    }
+    class Payment extends Transaction {
+        public Payment(LocalDate date, LocalTime time, String description, String vendor, float amount)  {
+            super(date, time, description, vendor, (double) amount);
+        }
     }
 }
--- AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/org/example/Main.java
+++ AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/org/example/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
        // Ledger ledger = new Ledger();
-        System.out.println("Hello world!");
+        System.out.println("Welcome ");
     }
 }
