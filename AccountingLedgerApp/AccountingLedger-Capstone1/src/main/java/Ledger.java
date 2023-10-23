527c1a02bbf1389f47077ab6b85c95acdcd55340
karyRmartinez <karymartinez@pursuit.org>
2023-10-23 18:10:35 -0400

    Reading Transaction from the file

--- AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/Ledger.java
+++ AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/Ledger.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +21,29 @@ public class Ledger {
     public List<Transaction> getTransactions() {
         return transactions;
     }
-
+     String fileName = "transactions.csv";
     public void readTransactionFile(String filename) {
+      DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+      DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
+      try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+          String line;
+          while ((line = reader.readLine()) != null) {
+              String[] fields = line.split("\\|");
+              LocalDate date = LocalDate.parse(fields[0], dateFormatter);
+              LocalTime time = LocalTime.parse(fields[1], timeFormatter);
+              String description = fields[2];
+              String vendor = fields[3];
+              float amount = Float.parseFloat(fields[4]);
+              Transaction transaction = new Transaction(date, time, description, vendor, (double) amount);
+              addTransaction(transaction);
+
+
+          }
+
+      } catch (IOException e) {
+          System.out.println("Error");
+
+      } ;
     }
 }
