dee0ba3275791b6866b5dd9ccceebe1976afed36
karyRmartinez <karymartinez@pursuit.org>
2023-10-24 09:35:52 -0400

    switch statement in Main class

--- AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/org/example/Main.java
+++ AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/org/example/Main.java
@@ -7,6 +7,63 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
        // Ledger ledger = new Ledger();
-        System.out.println("Welcome ");
+        System.out.println(" HOME ");
+        System.out.println("-------------------------");
+
+        while (true) {
+            System.out.println("Select option:");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment(Debit)");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            System.out.print("Enter your choice: ");
+            String choice = scanner.next().toUpperCase();
+
+            switch (choice) {
+                case "D":
+                    System.out.println("\nEnter deposit information:");
+                    System.out.print("Date (YYYY-MM-DD): ");
+                    String date = scanner.next();
+                    System.out.print("Time (HH:MM:SS): ");
+                    String time = scanner.next();
+                    System.out.print("Description: ");
+                    String description = scanner.next();
+                    System.out.print("Vendor: ");
+                    String vendor = scanner.next();
+                    System.out.print("Amount: ");
+                    double amount = scanner.nextDouble();
+                   // add deposit
+                    System.out.println("\nDeposit added!");
+                    break;
+                case "L":
+                    System.out.println("\nLedger Options:");
+                    System.out.println("A) All");
+                    System.out.println("D) Deposits");
+                    System.out.println("P) Payments");
+                    System.out.println("R) Reports");
+                    System.out.println("0) Back");
+                    System.out.println("H) Home");
+
+                    System.out.print("Please enter your choice: ");
+                    String ledgerChoice = scanner.next().toUpperCase();
+
+                    switch (ledgerChoice) {
+                        case "A":
+                            System.out.println("\nAll Transactions:");
+                           // add transactuion method
+                            //    then print it
+                            }
+                            break;
+
+
+                        case "0":
+                            break;
+
+                        default:
+                            System.out.println("Invalid choice. Please try again.");
+                        //might need a default
+            }
+
+        }
     }
 }
