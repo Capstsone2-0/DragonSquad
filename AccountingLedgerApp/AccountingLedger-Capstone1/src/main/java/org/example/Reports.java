3eb81ded31a2077972f7a241d07b77b234d92016
karyRmartinez <karymartinez@pursuit.org>
2023-10-26 10:34:53 -0400

    while loop that displays menu

--- AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/org/example/Reports.java
+++ AccountingLedgerApp/AccountingLedger-Capstone1/src/main/java/org/example/Reports.java
@@ -1,27 +1,50 @@
 package org.example;
+import java.util.List;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Reports {
+    public  void showReports(List<Transaction> transactions) {
+        while (true) {
 
-    private static String choices = "0";
+            System.out.println("Select option:");
+            System.out.println("1) Month To Date");
+            System.out.println("2) Previous Month)");
+            System.out.println("3) Year To Date");
+            System.out.println("4) Previous year");
+            System.out.print("5) Search by vendor: ");
+            //String choice = scanner.next().toUpperCase();
+            Scanner scanner = new Scanner(System.in);
+            int choices = scanner.nextInt();
+            LocalDate currentDate = LocalDate.now();
+            int currentMonth = currentDate.getMonthValue();
+            int currentYear = currentDate.getYear();
 
-    public static void showReports() {
-        switch (choices) {
-            case "1":
-                //monthToDate
-                break;
-            case "2":
-               // previousMonth
-                break;
-            case "3":
-               // yearToDate
-                break;
-            case "4":
-                //lastYearsTransactions
-                break;
-            case "5":
+
+//            private static String choices = "0";
+
+            switch (choices) {
+                case 1:
+
+                    //monthToDate
+                    break;
+                case 2:
+                    // previousMonth
+                    break;
+                case 3:
+                    // yearToDate
+                    break;
+                case 4:
+                    //lastYearsTransactions
+                    break;
+                case 5:
 //                TransactionsByVendor
+                case 6:
+                    break;
 
+            }
         }
-    }
 
+    }
 }
